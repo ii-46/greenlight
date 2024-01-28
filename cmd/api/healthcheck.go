9f3cf748b4dd47f1fae0ff884a6894d53f9669c8
Inthava XAIYALOUN <coci8750@gmail.com>
2024-01-28 14:30:43 +0700

    Fix healthcheckHandler to use Marshal instead of fixed JSON string

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	// The v parameter in the above method has the type interface{} (known as the
+	// empty interface). This effectively means that we’re able to pass any Go type to
+	// Marshal() as the v parameter.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write(js)
 }
