6b2d54449936e6e572c4f96a7167f3f9b8c0e033
Inthava XAIYALOUN <coci8750@gmail.com>
2024-01-28 13:18:27 +0700

    Add createMovieHandler and showMovieHandler to movies

---
+++ cmd/api/movies.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "show movie %d", id)
+}
