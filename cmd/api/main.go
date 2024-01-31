9b022a00e4cf6de9bde029f579b3cd974ab73b4c
Inthava XAIYALOUN <coci8750@gmail.com>
2024-01-31 10:15:10 +0700

    Add openDB helper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -14,6 +16,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dns string
+	}
 }
 
 type application struct {
@@ -25,10 +30,17 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dns, "db-dsn", "postgres://greenlight:greenlight@localhost/greenlight", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ldate)
 
+	db, err := openDb(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Println("database connection pool established")
 	app := application{
 		config: &cfg,
 		logger: logger,
@@ -43,6 +55,20 @@ func main() {
 	}
 
 	log.Printf("starting %s server on %d\n", cfg.env, cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func openDb(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dns)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
