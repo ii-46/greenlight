5994a2de18dce50cd8d616463a02a67399512110
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-14 20:26:09 +0700

    Extract filter from handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -167,19 +167,18 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title    string
-		Genres   []string
-		Page     int
-		PageSize int
-		Sort     string
+		Title  string
+		Genres []string
+		data.Filter
 	}
 	v := validator.New()
 	qs := r.URL.Query()
 
 	input.Title = app.readString(qs, "title", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
-	input.Sort = app.readString(qs, "sort", "id")
+	input.Filter.Page = app.readInt(qs, "page", 1, v)
+	input.Filter.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filter.Sort = app.readString(qs, "sort", "id")
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
