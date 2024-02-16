e902c41834f6bb7cbae04721d59975d945ccd65c
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-16 14:01:31 +0700

    Update server to use jsonlog

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -27,7 +27,7 @@ type envelop map[string]interface{}
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelop, header http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
-		app.logger.Println(err)
+		app.logger.PrintError(err, nil)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 		return err
 	}
