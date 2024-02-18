fb495f08d1331a8da3928098a17507d6f89db6d2
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-19 06:43:54 +0700

    Update Send to try again after failed

--- cmd/internal/mailer/mailer.go
+++ cmd/internal/mailer/mailer.go
@@ -51,9 +51,12 @@ func (m *Mailer) Send(recipient, templateFile string, data interface{}) error {
 	msg.SetHeader("Subject", subject.String())
 	msg.SetBody("text/plain", plainBody.String())
 	msg.AddAlternative("text/html", htmlBody.String())
-	err = m.dialer.DialAndSend(msg)
-	if err != nil {
-		return err
+	for i := 1; i <= 3; i++ {
+		err = m.dialer.DialAndSend(msg)
+		if nil == err {
+			return nil
+		}
+		time.Sleep(500 * time.Millisecond)
 	}
 	return nil
 }
