6a4746d4c4bccc9e65d525e58b9406df38d13312
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-18 07:15:02 +0700

    Add registerUserHandler and route

---
+++ cmd/api/users.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"greenlight.inthava.me/internal/data"
+	"greenlight.inthava.me/internal/validator"
+	"net/http"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+	v := validator.New()
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	err = app.models.User.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicatedEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusCreated, envelop{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
