4a5a8f6df1e868d50b7d9bd32dad9eca318d2803
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-19 11:00:40 +0700

    Update registerUserHandler to send email in go routine

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -43,11 +43,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	go func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+			return
+		}
+	}()
 	err = app.writeJSON(w, http.StatusCreated, envelop{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
