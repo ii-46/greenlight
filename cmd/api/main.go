001ee1735e4ee4cb04a6f739ea5dfe3089c786b2
Inthava XAIYALOUN <coci8750@gmail.com>
2024-02-19 11:17:48 +0700

    Update server to wait for completing background task in go routine

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"greenlight.inthava.me/internal/data"
 	"greenlight.inthava.me/internal/jsonlog"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -43,6 +44,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
